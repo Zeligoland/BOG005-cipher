b620677347742e8e4efbb71734019d2f44f53253
Andrea Zapata <95andreazapata@gmail.com>
2022-07-12 13:03:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import cipher from './cipher.js';
-let offset = getElementbyId() // Aquí van los elementos del DOM. De aquí se jalan las variables ingresadas.
+let offset = getElementbyId("offsetInput") // Aquí van los elementos del DOM. De aquí se jalan las variables ingresadas.
 console.log(cipher);
