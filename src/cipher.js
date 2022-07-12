c1b03b39ac4f0ffa370b09c4bd8dc66a0bb0caa7
Andrea Zapata <95andreazapata@gmail.com>
2022-07-12 13:00:28 -0500

    Decoding tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,14 @@
-const cipher = {
-  // ...
+const cipher.encode = {
+  var test = 'A'
+(test.charCodeAt() - 65 + 3) % 26 + 65 // Formula para obtener el codigo ASCII de la letra codificada con el offset ingresado  
+  
+  ('A'.charCodeAt() - 65 + n) % 26 + 65 // X = número de la letra ASCII obtenida con charCodeAt + n(Offset) 
+  
 };
+
+const cipher.decode = {
+
+
+}
 
 export default cipher;
