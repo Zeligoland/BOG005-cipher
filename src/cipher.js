43abd5d062b0f69dca8cbec8b16bdd9cd76f649e
Andrea Zapata <95andreazapata@gmail.com>
2022-07-14 13:08:30 -0500

    Hiding Tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,5 @@
-const cipher.encode = {
-  var test = 'A'
-(test.charCodeAt() - 65 + 3) % 26 + 65 // Formula para obtener el codigo ASCII de la letra codificada con el offset ingresado  
-  
-  ('A'.charCodeAt() - 65 + n) % 26 + 65 // X = número de la letra ASCII obtenida con charCodeAt + n(Offset) 
-  
+const cipher = {
+  // ...
 };
-
-const cipher.decode = {
-
-
-}
 
 export default cipher;
