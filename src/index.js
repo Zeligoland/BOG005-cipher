38df005e3421d92d84af94be8360a1c3ce45eb03
Andrea Zapata <95andreazapata@gmail.com>
2022-07-15 13:17:11 -0500

    Semantic HTML Adjustments
    Organización de la estructura final de la página en HTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import cipher from './cipher.js';
-let offset = getElementbyId("offsetInput"); // Aquí van los elementos del DOM. De aquí se jalan las variables ingresadas.;
+function mostrar(){
+    document.getElementById('h1').style.display = 'block';
+}
+
+function ocultar(){
+    document.getElementById('h1').style.display = 'none';
+}
+let botonMostrar = document.getElementById("boton");
+botonMostrar.addEventListener("click", mostrar);
+
+let botonOcultar = document.getElementById("botonDos");
+botonOcultar.addEventListener("click", ocultar);
+
+//let offset = getElementbyId("offsetInput"); // Aquí van los elementos del DOM. De aquí se jalan las variables ingresadas.;
 console.log(cipher);
